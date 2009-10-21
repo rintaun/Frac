1816d1b871baf0f8125eae6b84f9d51675de603e
Tony Young <rofflwaffls@gmail.com>
2009-10-22 10:19:54 +1300

    A few edits and fixes.

--- models/staff.php
+++ models/staff.php
@@ -18,11 +18,11 @@ class Staff extends Doctrine_Record
 	 * Columns for a staff member.
 	 *
 	 * id		-	integer(11)		-	Auto-incremeting ID primary key.
-	 * nickname	-	varchar(32)		-	Nickname for the staffer.
-	 * password	-	varchar(64)		-	SHA256 hash for the password (because MD5 is not safe :D)
-	 * comment	-	varchar(255)	-	Brief comment for the staffer.
-	 * email	-	varchar(255)	-	Email address of the staffer.
-	 * cell		-	varchar(32)		-	Cellphnoe number of the staffer.
+	 * nickname	-	string(32)		-	Nickname for the staffer.
+	 * password	-	string(64)		-	SHA256 hash for the password (because MD5 is not safe :D)
+	 * comment	-	string(255)	-	Brief comment for the staffer.
+	 * email	-	string(255)	-	Email address of the staffer.
+	 * cell		-	string(32)		-	Cellphnoe number of the staffer.
 	 * auth		-	integer(10)		-	Auth flags for the staffer.
 	 */
 	public function setTableDefinition()
@@ -37,13 +37,13 @@ class Staff extends Doctrine_Record
 			)
 		);
 		
-		$this->hasColumn("nickname", "varchar", 32, array(
+		$this->hasColumn("nickname", "string", 32, array(
 				"unique" => true,
 				"notnull" => true
 			)
 		);
 		
-		$this->hasColumn("password", "varchar", 64, array(
+		$this->hasColumn("password", "string", 64, array(
 				"regexp" => "/^[0-9a-f]{60}\$/i",
 				"notnull" => true,
 				"fixed" => true
@@ -52,12 +52,12 @@ class Staff extends Doctrine_Record
 		
 		$this->hasColumn("comment", "string", 1000);
 		
-		$this->hasColumn("email", "varchar", 255, array(
+		$this->hasColumn("email", "string", 255, array(
 				"regexp" => "/^[0-9a-f_-.+]+@[0-9a-z_-]+.[0-9a-z_-.]+\$/i"
 			)
 		);
 		
-		$this->hasColumn("cell", "varchar", 32, array(
+		$this->hasColumn("cell", "string", 32, array(
 				"regexp" => "/^[0-9]*\$/"
 			)
 		);
