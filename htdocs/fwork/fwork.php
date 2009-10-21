dedc4755f93085f703065ec8e4e00b41826c4978
Tony Young <rofflwaffls@gmail.com>
2009-10-21 18:30:50 +1300

    Doxygen documentation and MIT license.

---
+++ htdocs/fwork/fwork.php
@@ -0,0 +1,32 @@
+<?
+/*
+ * Fwork
+ * Copyright (c) 2009 Matthew Lanigan
+ *                    Tony Young
+ *
+ * See COPYING for license conditions.
+ */
+
+if(!defined("IN_FWORK_")) die("This file cannot be invoked directly.");
+
+/**
+ * Fwork core class.
+ *
+ * This class is the core of the Fwork framework.
+ */
+class Fwork
+{
+    /**
+     * Constructor for Fwork.
+     *
+     * Currently does nothing.
+     */
+    public function __construct() { }
+    
+    /**
+     * Destructor for Fwork.
+     *
+     * Currently also does nothing.
+     */
+    public function __destruct() { }
+}
