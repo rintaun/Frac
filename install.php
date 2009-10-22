61b4ffb45c0e54d054ec3a609e2e9aac0ca151ae
Tony Young <rofflwaffls@gmail.com>
2009-10-22 14:04:57 +1300

    Moved install/index.php to install.php.

--- install.php
+++ install.php
@@ -10,11 +10,11 @@
 define("IN_FRAC_", true);
 define("IN_FWORK_", true);
 
-require_once(dirname(__FILE__) . "/../config.php"); // eventually this will be user-input generated
+require_once(dirname(__FILE__) . "/config.php"); // eventually this will be user-input generated
 
-require_once(dirname(__FILE__) . "/../lib/Doctrine/Doctrine.php");
+require_once(dirname(__FILE__) . "/lib/Doctrine/Doctrine.php");
 spl_autoload_register(array("Doctrine", "autoload"));
 
 Doctrine_Manager::connection($config["database"]["dsn"]);
 Doctrine_Manager::getInstance()->setAttribute(Doctrine::ATTR_TBLNAME_FORMAT, $config["database"]["prefix"] . "%s");
-Doctrine::createTablesFromModels(dirname(__FILE__) . "/../models");
+Doctrine::createTablesFromModels(dirname(__FILE__) . "/models");
