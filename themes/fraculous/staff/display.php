5d66c7bdf1a5a8b8460b904a55b46a204ecbf495
Nolan Lum <nol888@gmail.com>
2009-10-27 21:05:08 -0400

    Some more gravatar insertion, use more divs and less uglyness.

--- themes/fraculous/staff/display.php
+++ themes/fraculous/staff/display.php
@@ -1,10 +1,18 @@
-<h3><?php $this->eprint($this->user->nickname); ?></h3>
-<p><?php $this->eprint($this->user->comment); ?></p>
-<ul>
-	<li>E-mail: <?php $this->eprint($this->user->email); ?></li>
+<div class="userdetails">
+	<div class="header">
+		<img alt="" width="75" height="75" src="http://www.gravatar.com/avatar/<?php echo md5($this->user->email); ?>?size=75" />
+		<p>
+			<span class="title"><?php $this->eprint($this->user->nickname); ?></span><br />
+			<span class="comment"><?php $this->eprint($this->user->comment); ?></span>
+		</p>
+	</div>
+	
+	<ul>
+		<li>E-mail: <?php $this->eprint($this->user->email); ?></li>
 <?php if(empty($this->user->cell)): ?>
-	<li>No mobile number available for this staff member.</li>
+		<li>No mobile number available for this staff member.</li>
 <?php else: ?>
-	<li>Mobile: <?php $this->eprint($this->user->cell); ?></li>
+		<li>Mobile: <?php $this->eprint($this->user->cell); ?></li>
 <?php endif; ?>
-</ul>
+	</ul>
+</div>
--- themes/fraculous/staff/index.php
+++ themes/fraculous/staff/index.php
@@ -1,8 +1,10 @@
 <p><span class="title">Staff List</span></p>
 <?php foreach($this->staff as $member): ?>
 <div class="user">
-	<img alt="" width="32" height="32" src="http://www.gravatar.com/avatar/<?php echo md5($member[2]); ?>?rating=PG&amp;size=32" />
-	<a href="<?php $this->eprint($this->frac->createuri('staff/display/' . $member[0])); ?>"><?php $this->eprint($member[1]); ?></a>
+	<a href="<?php $this->eprint($this->frac->createuri('staff/display/' . $member[0])); ?>">
+		<img alt="" width="32" height="32" src="http://www.gravatar.com/avatar/<?php echo md5($member[2]); ?>?size=32" />
+		<?php $this->eprint($member[1]); ?>
+	</a>
 	<p>Actions: <a href="<?php $this->eprint($this->frac->createuri('staff/edit/' . $member[0])); ?>">Edit</a>, <a href="<?php $this->eprint($this->frac->createuri('staff/edit/' . $member[0])); ?>">Delete</a></p>
 </div>
 <?php endforeach; ?>
